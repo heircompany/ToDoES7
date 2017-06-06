7f4abb0033336863680c5132dea2c103b0eebf07
Joe Grotto <joegrotto@heir.company>
2017-06-06 17:28:55 -0500

    final

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_6zrgk8xg:CUrocOnjohahum5@ds111922.mlab.com:11922/heroku_6zrgk8xg');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
