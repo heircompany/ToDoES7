c53ba25d63aa61894a1b9e2994afff44345df1b2
Joe Grotto <joegrotto@heir.company>
2017-06-06 16:52:02 -0500

    final

--- server/config/config.js
+++ server/config/config.js
@@ -7,4 +7,6 @@ if (env === 'development' || env === 'test') {
   Object.keys(envConfig).forEach((key) => {
     process.env[ key ] = envConfig[ key ];
   });
+} else {
+  process.env.MONGODB_URI = 'mongodb://heroku_6zrgk8xg:CUrocOnjohahum5@ds111922.mlab.com:11922/heroku_6zrgk8xg'
 }
